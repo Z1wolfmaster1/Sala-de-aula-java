b91af26d98d4c11bd321e32ae9acaf56113f9db1
Z1wolfmaster1 <suiciniv2016@outlook.com>
2018-04-19 08:34:56 -0300

    Add files via upload

---
+++ Majuuj.java
@@ -0,0 +1,17 @@
+package Jorcional_Nacional;
+
+import java.util.*;
+
+public class Majuuj {
+    public static void main(String args[]){
+        ContaBancaria n1=new ContaBancaria();
+        ContaBancaria n2=new ContaBancaria();
+        Scanner input=new Scanner(System.in);
+        float x;
+        int i;
+        
+        for(;;){
+            System.out.printf("1. Sacar\n2. Depositar\n3. Ver Saldo\n4. Finalizar");
+        }
+    }
+}
