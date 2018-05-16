34ed54df743382b08c4bc381fdf87d6a5ae7ba9d
Z1wolfmaster1 <suiciniv2016@outlook.com>
2018-05-16 10:35:15 -0300

    Add files via upload

---
+++ Pessoa_Construtores.java
@@ -0,0 +1,15 @@
+package Construtores;
+
+import Tarefa.Endereco;
+import java.util.Scanner;
+
+public class Pessoa {
+    Scanner input=new Scanner(System.in);
+    String nome, cpf;
+    Endereco ende=new Endereco();
+    
+    void cadastrar(){
+        System.out.printf("");
+    }
+    
+}
---
+++ Principal_Construtores.java
@@ -0,0 +1,5 @@
+package Construtores;
+
+public class Principal {
+    
+}
